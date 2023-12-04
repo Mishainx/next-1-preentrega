9f255ec68034d2f6e1eb842e486e76fc33e6ee39
mishainx <fer.r@live.com.ar>
2023-12-04 17:39:17 -0300

    modificaciones

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 export default{
     API_KEY: process.env.apikey,
     AUTH_DOMAIN: process.env.autuDomain,
-    PROJECT_ID: process.env.projectID,
+    PROJECT_ID: process.env.projectId,
     STORAGE_BUCKET: process.env.storageBucket,
     MESSAGING_SENDER_ID: process.env.messaggingSenderId,
     APP_ID: process.env.appId
