bab2f154a438f1720bf666b3b3305d192cc9dde2
mishainx <fer.r@live.com.ar>
2023-12-20 11:58:27 -0300

    modificaciones

--- app/components/forms/RegisterForm.js
+++ app/components/forms/RegisterForm.js
@@ -1,120 +1,162 @@
-"use client"
+"use client";
 
-import Button from "@/app/components/ui/Button"
+import Button from "@/app/components/ui/Button";
 import { useState } from "react";
 import { useAuthContext } from "../context/AuthContext";
-import {isValidName,isValidSurname,isValidTelephone,isValidEmail, isValidPassword} from "@/src/utils";
+import {
+  isValidName,
+  isValidSurname,
+  isValidTelephone,
+  isValidEmail,
+  isValidPassword,
+} from "@/src/utils";
 import { toast } from "react-toastify";
 
+const toastNotifyErrorForm = (error) =>
+  toast(` Error al crear el usuario: ${error} inválido `, {
+    hideProgressBar: true,
+    autoClose: 2000,
+    type: "error",
+  });
 
-const toastNotifyError = () => toast('Error al crear el usuario', { hideProgressBar: true, autoClose: 2000, type: 'error' })
-const toastNotifyErrorForm= (error) => toast( ` Error al crear el usuario: ${error} inválido ` , { hideProgressBar: true, autoClose: 2000, type: 'error' })
+const RegisterForm = () => {
+  const fieldsToValidate = [
+    { name: "name", validator: isValidName, errorMessage: "Nombre inválido" },
+    {
+      name: "surname",
+      validator: isValidSurname,
+      errorMessage: "Apellido inválido",
+    },
+    {
+      name: "telephone",
+      validator: isValidTelephone,
+      errorMessage: "Teléfono inválido",
+    },
+    { name: "email", validator: isValidEmail, errorMessage: "Email inválido" },
+    {
+      name: "password",
+      validator: isValidPassword,
+      errorMessage: "Contraseña inválida",
+    },
+  ];
 
-const RegisterForm = () =>{
+  const { createUser } = useAuthContext();
+  const [values, setValues] = useState({
+    name: "",
+    surname: "",
+    telephone: 0,
+    email: "",
+    password: "",
+    cart: "",
+    tickets: "",
+    role: "",
+  });
 
-    const fieldsToValidate = [
-        { name: "name", validator: isValidName, errorMessage: "Nombre inválido" },
-        { name: "surname", validator: isValidSurname, errorMessage: "Apellido inválido" },
-        { name: "telephone", validator: isValidTelephone, errorMessage: "Teléfono inválido" },
-        { name: "email", validator: isValidEmail, errorMessage: "Email inválido" },
-        { name: "password", validator: isValidPassword, errorMessage: "Contraseña inválida" },
-      ];
-
-    const { createUser } = useAuthContext();
-    const [values, setValues] = useState({
-      name: "",  
-      surname: "",
-      telephone: 0,
-      email: "",
-      password: "",
-      cart: "",
-      tickets:"",
-      role:""
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
     });
+  };
 
-    const handleChange = (e) => {
-        setValues({
-          ...values,
-          [e.target.name]: e.target.value,
-        });
-      };
-
-      const handleSubmit = async (e) => {
-        e.preventDefault();
-            // Validaciones
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     for (const field of fieldsToValidate) {
-        const isValid = field.validator(values[field.name]);
-        if (!isValid) {
-          toastNotifyErrorForm(field.errorMessage)
-          return;
-        }
+      const isValid = field.validator(values[field.name]);
+      if (!isValid) {
+        toastNotifyErrorForm(field.errorMessage);
+        return;
       }
+    }
 
-      await createUser(values)
+    await createUser(values);
+  };
 
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="w-full max-w-md p-6 bg-white rounded-md shadow-md border border-amber-400">
+        <h2 className="text-2xl font-bold mb-4">Registro</h2>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div className="flex flex-col">
+            <label htmlFor="name" className="text-sm font-semibold mb-1">
+              Nombre:
+            </label>
+            <input
+              required
+              type="text"
+              id="name"
+              name="name"   
+              className="input-field border border-amber-400"
+              value={values.name}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="surname" className="text-sm font-semibold mb-1">
+              Apellido:
+            </label>
+            <input
+              required
+              type="text"
+              id="surname"
+              name="surname"
+              className="input-field border border-amber-400"
+              value={values.surname}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="telephone" className="text-sm font-semibold mb-1">
+              Teléfono:
+            </label>
+            <input
+              required
+              type="number"
+              id="telephone"
+              name="telephone"
+              className="input-field border border-amber-400"
+              value={values.telephone}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="email" className="text-sm font-semibold mb-1">
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="input-field border border-amber-400"
+              value={values.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex flex-col ">
+            <label htmlFor="password" className="text-sm font-semibold mb-1">
+              Contraseña:
+            </label>
+            <input
+              required
+              type="password"
+              id="password"
+              name="password"
+              placeholder=""
+              className="input-field border border-amber-400"
+              value={values.password}
+              onChange={handleChange}
+            />
+          </div>
+          <Button
+            className="bg-amber-400 rounded-md p-2 text-white hover:bg-amber-500"
+            type="submit"
+          >
+            Enviar
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-
-      }; 
-
-    return(
-        <div className="h-90 py-8 flex justify-center">
-            <form className="flex flex-col  p-5 justify-center items-center border-amber-400 border-2 gap-2 text-xs"
-            onSubmit={handleSubmit}
-            >
-                <input 
-                    required
-                    type="text" 
-                    name="name" 
-                    placeholder="Nombre" 
-                    className="h-5 border-2 border-amber-400"
-                    value={values.name}
-                    onChange={handleChange}
-
-                />
-                <input 
-                    required
-                    type="text" 
-                    name="surname" 
-                    placeholder="Apellido" 
-                    className="h-5 border-2 border-amber-400"
-                    value={values.surname}
-                    onChange={handleChange}
-                />
-                <input 
-                    required
-                    type="number" 
-                    name="telephone" 
-                    placeholder="Telefono" 
-                    className="h-5 border-2 border-amber-400"
-                    value={values.telephone}
-                    onChange={handleChange}
-                />
-                <input
-                    type='email'
-                    placeholder="Email"
-                    name="email"
-                    className="h-5 border-2 border-amber-400"
-                    value={values.email}
-                    onChange={handleChange}
-                 />
-                <input 
-                    required
-                    type='password'
-                    placeholder="Contraseña"
-                    name="password"
-                    className="h-5 border-2 border-amber-400"
-                    value={values.password}
-                    onChange={handleChange}
-                    />
-                <Button
-                className="bg-amber-400 rounded-md p-1 text-xs hover:text-white"
-                type="submit"
-                >
-                    Enviar
-                </Button>
-            </form>
-        </div>
-    )
-}
-
-export default RegisterForm
+export default RegisterForm;
