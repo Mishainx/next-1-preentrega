a65a17560ee5a7f63f1fd7a9c5d98427addd4d55
mishainx <fer.r@live.com.ar>
2023-12-20 12:32:59 -0300

    modificaciones

--- app/admin/product/[slug]/page.js
+++ app/admin/product/[slug]/page.js
@@ -5,7 +5,7 @@ export default async function EditProduct({params}) {
 
   
     const {slug} = params
-    const response = await fetch(`http://localhost:3000/api/product/${slug}`,
+    const response = await fetch(`http://${process.env.VERCEL_URL}/api/product/${slug}`,
     {
         cache:"no-store"
     })
