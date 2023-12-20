923815ee6956ba126e4b19faefac6cf0b4599215
mishainx <fer.r@live.com.ar>
2023-12-20 20:33:56 -0300

    modificaciones

--- app/page.js
+++ app/page.js
@@ -8,11 +8,16 @@ export const metadata ={
 }
 
 async function getItems(){
-  const response = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/products`,{cache:"no-cache"})
-  if (response.ok) {
-    return response.json();
-  } else {
-    console.log("Error en la respuesta:", response.status);
+  try{
+    const response = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/products`,{cache:"no-cache"})
+    if (response.ok) {
+      return response.json();
+    } else {
+      console.log("Error en la respuesta:", response.status);
+    }
+  }
+  catch(error){
+    throw new Error("failed to fetch")
   }
 }
 
