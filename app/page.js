2eebc7f7134b35c6ba396be03f61a94c7eadefd8
mishainx <fer.r@live.com.ar>
2023-12-20 20:25:31 -0300

    modificaciones

--- app/page.js
+++ app/page.js
@@ -1,37 +1,26 @@
-"use client"
-
-import { useEffect, useState } from 'react';
 import ItemList from "./components/products/ItemList";
 import CategoriesShow from "./components/categories-show/CategoriesShow";
 
-export const metadata = {
-  title: "Mate y venga",
+export const metadata ={
+  title:"Mate y venga",
   description: "Tienda de mates",
-  keywords: ["mate", "yerba", "bombilla", "tradición argentina"],
-};
+  keywords: ["mate","yerba","bombilla","tradición argentina"],
+}
 
-export default function Home() {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    async function fetchProducts() {
-      const response = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/products`, { cache: "no-cache" });
-      
-      if (response.ok) {
-        const fetchedItems = await response.json();
-        setItems(fetchedItems);
-      } else {
-        console.error("Error en la respuesta:", response.status);
-      }
-    }
-
-    fetchProducts();
-  }, []);
+export default async function Home() {
+  const response = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/products`,{cache:"no-cache"})
+  if (response.ok) {
+    const items = await response.json();
+  } else {
+    console.log("Error en la respuesta:", response.status);
+  }
+  const items = await response.json()
+  console.log(items, "consoleado")  
 
   return (
     <main className="min-h-screen	flex flex-col justify-center items-center">
-      <CategoriesShow />
-      <ItemList items={items} />
+              <CategoriesShow/>
+              <ItemList items={items}/>
     </main>
-  );
+  )
 }
