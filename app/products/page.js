68c245d297f68cbc7f406890d070e699b018bf56
mishainx <fer.r@live.com.ar>
2023-12-20 20:54:04 -0300

    modificaciones

--- app/products/page.js
+++ app/products/page.js
@@ -10,11 +10,22 @@ export const generateMetadata = async () => {
 
 export default async function Products() {
 
-  const response = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/products`,
-  {
-    cache: "no-cache"
-})
-  const items = await response.json()
+  async function getItems(){
+    try{
+      const response = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/products`,{cache:"no-cache"})
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.log(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/products`)
+        console.log("Error en la respuesta:", response.status);
+      }
+    }
+    catch(error){
+      throw new Error("failed to fetch")
+    }
+  }
+  
+  const items = await getItems()
 
     return (
       <main className="">
