74ec6532a6389c7477011fecac98d3350225046c
mishainx <fer.r@live.com.ar>
2023-12-21 01:02:40 -0300

    modificaciones

--- app/product/[slug]/page.js
+++ app/product/[slug]/page.js
@@ -8,6 +8,23 @@ export const generateMetadata = async ({params}) => {
   }
 }
 
+async function fetchProduct(slug) {
+    try {
+        const response = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/product/${slug}`, {
+            cache: "no-store"
+        });
+
+        if (response.ok) {
+            return await response.json();
+        } else {
+            console.error(`Error en la solicitud: ${response.status}`);
+            return null;
+        }
+    } catch (error) {
+        console.error('Error en la obtención del producto:', error);
+        return null;
+    }
+}
 
 export default async function ProductDetail({params}) {
     const {slug} = params
