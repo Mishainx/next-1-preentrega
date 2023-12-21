04f669b8730cc86d8391d2882ad3c0969089e737
mishainx <fer.r@live.com.ar>
2023-12-21 10:24:10 -0300

    modificaciones

--- app/components/ui/Header/Header.js
+++ app/components/ui/Header/Header.js
@@ -3,7 +3,6 @@ import Logo from "../Logo"
 
 const Header = () => {
 
-    return (
         return (
             <header className="w-full bg-amber-400">
               <div className="container mx-auto py-3 md:py-4 flex justify-between items-center">
