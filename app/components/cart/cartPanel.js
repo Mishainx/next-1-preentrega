bbc46e056a9429dbae070b761f8083019fafe04c
mishainx <fer.r@live.com.ar>
2023-12-21 09:27:50 -0300

    modificaciones

--- app/components/cart/cartPanel.js
+++ app/components/cart/cartPanel.js
@@ -18,12 +18,15 @@ const CartPanel = async() => {
   return (
     <section className="w-full flex flex-col border-2 border-amber-400">
       <div className="w-full flex flex-row justify-end p-2">
-        <button className="bg-amber-400 text-white py-1 px-3 rounded hover:scale-105 duration-200 hover:text-gray-500" onClick={() => setShowTickets(!showTickets)}>
-        {showTickets ? 'Ver Carrito' : 'Ver Tickets'}
+        <button
+          className="bg-amber-400 text-white py-1 px-3 rounded hover:scale-105 duration-200 hover:text-gray-500"
+          onClick={() => setShowTickets(!showTickets)}
+        >
+          {showTickets ? 'Ver Carrito' : 'Ver Tickets'}
         </button>
       </div>
-      <div className="w-full flex flex-row border-2 border-amber-400">
-        <div className="w-44 flex flex-col justify-center items-center  border-r-2 border-amber-400">
+      <div className="w-full flex flex-col md:flex-row border-2 border-amber-400">
+        <div className="w-full md:w-44 flex flex-col justify-center items-center md:border-r-2 border-amber-400">
           <Image alt="user icon" src={userImage} width={90} height={90} />
           <p>
             {userData.name} {userData.surname}
@@ -31,12 +34,11 @@ const CartPanel = async() => {
           <p>{userData.email}</p>
           <p>{userData.telephone}</p>
         </div>
-          <div className="w-full flex flex-col justify-center items-center border-amber-400">
-            {showTickets ? <TicketList /> : <CartList cart={cart.items} removeFromCart={removeFromCart} buyCart={buyCart} getTicket={getTicket} />}
-          </div>
+        <div className="w-full flex flex-col justify-center items-center border-amber-400">
+          {showTickets ? <TicketList /> : <CartList cart={cart.items} removeFromCart={removeFromCart} buyCart={buyCart} getTicket={getTicket} />}
+        </div>
       </div>
     </section>
   );
-};
 
 export default CartPanel;
