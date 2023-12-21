a47e78a6c8ad0b2785910b079353e8c55e29ffe4
mishainx <fer.r@live.com.ar>
2023-12-21 10:21:03 -0300

    modificaciones

--- app/components/ui/Header/Header.js
+++ app/components/ui/Header/Header.js
@@ -4,8 +4,9 @@ import Logo from "../Logo"
 const Header = () => {
 
     return (
-        <header className="w-full bg-amber-400">
-            <div className="container m-auto py-3 flex justify-between items-center">
+        return (
+            <header className="w-full bg-amber-400">
+              <div className="container mx-auto py-3 md:py-4 flex justify-between items-center">
             <Logo/>
             <Menu/>
             </div>
